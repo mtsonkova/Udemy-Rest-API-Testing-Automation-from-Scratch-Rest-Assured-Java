9b5994364f6f03f3bf112e2aaaedf482aae74443
mtsonkova <mtsonkova950@gmail.com>
2024-02-18 23:05:35 +0200

    Added Json Simple dependency. Test cases for GET and POST requests are written.

--- src/test/java/Tests/TestExamples.java
+++ src/test/java/Tests/TestExamples.java
@@ -4,17 +4,24 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.json.simple.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+
 public class TestExamples {
+
+    private String base = "https://reqres.in/api";
+
     @Test
 
     public void TestGetStatusCodeEquals200() {
 
         Response response = get("https://reqres.in/api/users?page=2");
-        int statusCode =  response.getStatusCode();
+        int statusCode = response.getStatusCode();
         String statusLine = response.getStatusLine();
         String contentType = response.getContentType();
         String respBody = response.getBody().asPrettyString();
@@ -26,17 +33,33 @@ public class TestExamples {
 
     @Test
 
-    public void Test2() {
-        baseURI = "https://reqres.in/api";
-        given().get("/users?page=2")
-                .then().statusCode(200)
-                .body("data.id[1]", equalTo(1));
+    public void TestGET() {
+        baseURI = base;
+        given()
+           .get("/users?page=2")
+        .then()
+           .statusCode(200)
+           .body("data.id[1]", equalTo(1))
+           .body("data[4].first_name", equalTo("George"))
+           .body("data.first_name", hasItems("Rachel", "Michael", "Lindsay"));
 
     }
 
     @Test
-    public void testGET() {
+    public void testPOST() {
+        baseURI = base;
 
+        JSONObject request = new JSONObject();
+        request.put("name", "Morpheus");
+        request.put("job", "leader");
+
+        given().
+                contentType(ContentType.JSON)
+                .body(request.toJSONString()).
+        when()
+                .post("/users").
+        then()
+                .statusCode(201);
     }
 
 }
