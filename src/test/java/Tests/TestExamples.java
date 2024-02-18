5307363c00b61f49d285e583cdf961b8e6b4f950
mtsonkova <mtsonkova950@gmail.com>
2024-02-18 17:00:34 +0200

    In progress

--- src/test/java/Tests/TestExamples.java
+++ src/test/java/Tests/TestExamples.java
@@ -1,15 +1,42 @@
 package Tests;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestExamples {
     @Test
 
-    public void Test_One() {
-        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+    public void TestGetStatusCodeEquals200() {
+
+        Response response = get("https://reqres.in/api/users?page=2");
+        int statusCode =  response.getStatusCode();
+        String statusLine = response.getStatusLine();
+        String contentType = response.getContentType();
+        String respBody = response.getBody().asPrettyString();
+        System.out.println(respBody);
+        Assert.assertEquals(statusCode, 200);
+        Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+        Assert.assertEquals(contentType, "application/json; charset=utf-8");
     }
 
+    @Test
+
+    public void Test2() {
+        baseURI = "https://reqres.in/api";
+        given().get("/users?page=2")
+                .then().statusCode(200)
+                .body("data.id[1]", equalTo(1));
+
+    }
+
+    @Test
+    public void testGET() {
+
+    }
 
 }
