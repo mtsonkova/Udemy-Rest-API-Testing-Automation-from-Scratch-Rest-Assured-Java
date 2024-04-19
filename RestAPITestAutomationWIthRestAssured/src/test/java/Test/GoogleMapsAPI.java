2405c82a1112159324f06cab022803b128ebf36e
mtsonkova <mtsonkova950@gmail.com>
2024-04-19 21:38:15 +0300

    Test case updated to fetch Json response in a variable and print it

--- RestAPITestAutomationWIthRestAssured/src/test/java/Test/GoogleMapsAPI.java
+++ RestAPITestAutomationWIthRestAssured/src/test/java/Test/GoogleMapsAPI.java
@@ -1,45 +1,45 @@
 package Test;
 
+import Files.payload;
 import io.restassured.RestAssured;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 
 public class GoogleMapsAPI {
 
     @BeforeMethod(alwaysRun = true)
-   public void setUp() {
+    public void setUp() {
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-   }
+    }
 
-   // given() has all input details
+
+    // given() has all input details
     // when() has the HTTP method and the resource / endpoint
     // given().log().all() -> lets us see how the parameters are send;
     //then();log().all() -< let us see the received response.
+    // to extract the response of the server you use .extract().response().asString
 
-   @Test
+    @Test
     public void testAddNewLocation() {
-    given().log().all().queryParam("key", "qaclick123")
-            .header("Content-Type", "application/json")
-            .body("{\n" +
-                    "  \"location\": {\n" +
-                    "    \"lat\": -38.383494,\n" +
-                    "    \"lng\": 33.427362\n" +
-                    "  },\n" +
-                    "  \"accuracy\": 50,\n" +
-                    "  \"name\": \"Frontline house\",\n" +
-                    "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                    "  \"address\": \"29, side layout, cohen 09\",\n" +
-                    "  \"types\": [\n" +
-                    "    \"shoe park\",\n" +
-                    "    \"shop\"\n" +
-                    "  ],\n" +
-                    "  \"website\": \"http://google.com\",\n" +
-                    "  \"language\": \"French-IN\"\n" +
-                    "}")
-            .when().post("maps/api/place/add/json")
-            .then().log().all().assertThat().statusCode(200);
-   }
+        String response = given().log().all().queryParam("key", "qaclick123")
+                .header("Content-Type", "application/json")
+                .body(payload.AddPlace())
+                .when().post("maps/api/place/add/json")
+                .then()
+                .assertThat().statusCode(200)
+                .body("scope", equalTo("APP"))
+                .body("status", equalTo("OK"))
+                .header("Server", equalTo("Apache/2.4.52 (Ubuntu)"))
+                .extract().response().asString();
+
+        System.out.println(response);
+    }
+
+
+
+    //Add place -> Update place with address -> Get place to validate if new address is presence in response.
 }
