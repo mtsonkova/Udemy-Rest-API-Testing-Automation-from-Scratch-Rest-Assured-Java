738b9c3ff98c365d629754fd8aa81e79c75ffa39
mtsonkova <mtsonkova950@gmail.com>
2024-05-14 22:39:35 +0300

    In progress

--- RestAPITestAutomationWIthRestAssured/src/test/java/OAuthAPITest/Courses.java
+++ RestAPITestAutomationWIthRestAssured/src/test/java/OAuthAPITest/Courses.java
@@ -1,5 +1,8 @@
 package OAuthAPITest;
 
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -11,13 +14,28 @@ public class Courses {
 
     @Test
     public void getCoursesDetails() {
-        String token = given().header("Content-Type", "multipart/form-data")
+        // get access token
+
+        Response response = given()
                 .formParam( "client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
                 .formParam(" client_secret", "erZOWM9g3UtwNRj340YYaK_W")
                 .formParam("grant_type", "client_credentials")
                 .formParam("scope", "trust")
-                .when().post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token")
-                .then().log().all().assertThat().statusCode(200).extract().response().asPrettyString();
+                .when().post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token");
 
+        String accessToken = response.getBody().jsonPath().getString("access_token");
+
+        // get courses details
+
+        Response bookDetails = given().queryParam("access_token", accessToken)
+                .when().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails");
+        String bookDetailsContent = bookDetails.asPrettyString();
+        System.out.println(bookDetailsContent);
+        int statusCode = bookDetails.getStatusCode();
+        System.out.println(statusCode);
+
+        Assert.assertTrue(statusCode == 200);
+        Assert.assertFalse(bookDetailsContent.isEmpty());
+        Assert.assertTrue(bookDetailsContent.contains("Appium-Mobile Automation using Java"));
     }
 }
